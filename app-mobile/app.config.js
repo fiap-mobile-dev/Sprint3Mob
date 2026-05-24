2efa5b7565d30ac7152090b343741695f9b987c2
Gustavo <tyul150206@gmail.com>
2026-05-24 16:15:06 -0300

    Ajusta build final Android

--- app-mobile/app.config.js
+++ app-mobile/app.config.js
@@ -19,6 +19,10 @@ module.exports = ({ config }) => ({
   name: 'OracleLearn',
   slug: 'oraclelearn',
   scheme: 'oraclelearn',
+  android: {
+    ...(config.android || {}),
+    package: 'com.guuhh12.oraclelearn',
+  },
   extra: {
     ...(config.extra || {}),
     apiUrl: process.env.EXPO_PUBLIC_API_URL || 'http://localhost:3000/api',
